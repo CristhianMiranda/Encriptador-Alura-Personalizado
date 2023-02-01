89fd16307260c72d225a899ac7c24b73cda42b40
Cristhian <cristhianandres2001@gmail.com>
2023-01-31 23:53:06 -0500

    Pagina completa

--- docs/scripts/matrix.js
+++ docs/scripts/matrix.js
@@ -60,3 +60,88 @@ document.getElementById("mensaje").addEventListener("click", function() {
 		this.value = "";
 	}
 	});
+
+
+
+	const images = document.querySelectorAll("#image-container img");
+	const button = document.querySelector("button");
+	let currentIndex = 0;
+	
+	setInterval(() => {
+		images[currentIndex].style.display = "none";
+		currentIndex = (currentIndex + 1) % images.length;
+		images[currentIndex].style.display = "block";
+	  }, 2000);
+
+
+	  const mensaje = document.getElementById("mensaje");
+	  const encriptador = document.getElementById("encriptador");
+	  const desencriptador = document.getElementById("desencriptador");
+	  const output = document.getElementById("output");
+	  const imageContainer = document.getElementById("image-container");
+	  
+	  encriptador.addEventListener("click", function() {
+		const encrypted = encrypt(mensaje.value);
+		output.innerText = encrypted;
+		imageContainer.style.opacity = 0;
+	  });
+	  
+	  desencriptador.addEventListener("click", function() {
+		const decrypted = decrypt(mensaje.value);
+		output.innerText = decrypted;
+		imageContainer.style.opacity = 0;
+	  });
+	  
+
+
+
+
+
+	  function encrypt(word) {
+		let encrypted = "";
+		for (let i = 0; i < word.length; i++) {
+		  switch (word[i]) {
+			case "e":
+			  encrypted += "enter";
+			  break;
+			case "i":
+			  encrypted += "imes";
+			  break;
+			case "a":
+			  encrypted += "ai";
+			  break;
+			case "o":
+			  encrypted += "ober";
+			  break;
+			case "u":
+			  encrypted += "ufat";
+			  break;
+			default:
+			  encrypted += word[i];
+			  break;
+		  }
+		}
+		return encrypted;
+	  }
+	  
+	  function decrypt(str) {
+		return str.replace(/enter/g, 'e')
+				  .replace(/imes/g, 'i')
+				  .replace(/ai/g, 'a')
+				  .replace(/ober/g, 'o')
+				  .replace(/ufat/g, 'u');
+	  }
+	  
+
+
+
+
+
+
+
+
+
+
+
+
+	  
